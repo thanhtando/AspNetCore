3ef4ae3fb752fcca04d02bf43890673d9c00d5dc
Tan Do <thanhtando65@gmail.com>
2021-12-24 00:43:27 +0900

    [2021/12/23 dotan] create init api web and client

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.scss';
+import Examples from './examples';
+
+function App() {
+  return (
+    <div className="App">
+      <Examples />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/examples/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Examples = () => {
+  return(
+    <div>
+      <label> example </label>
+    </div>
+  )
+}
+export default Examples;
