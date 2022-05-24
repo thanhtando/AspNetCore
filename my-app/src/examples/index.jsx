7d4e20f4b16a4286022859e446b86a5eabeb6e56
Tan Do <thanhtando65@gmail.com>
2022-05-24 20:14:23 +0900

    new setting file

--- my-app/src/examples/index.jsx
+++ my-app/src/examples/index.jsx
@@ -13,22 +13,25 @@ import ContextRed from "./contextReducer";
 import StyleTest from "./teststyle";
 
 const Example = () => {
+  const test = process.env.PROJECT_ID;
+  console.log("test environment:", test);
   return(
     <div>
-      <StyleTest />
+      <label>abc_{process.env.PROJECT_ID}</label>
+      {/* <StyleTest />
       <ContextRed />
       <ThemeProExt />
       <TemporaryDrawer />
-        <StyleComponent />
+        <StyleComponent /> */}
         {/* <AlertTT /> */}
         {/* <ReduxExample /> */}
-        <ContextEx />
+        {/* <ContextEx />
         <ContextChange />
         <ContextBasic />
         <ContextNone />
         <MemoBasic/>
         <ForWardRefBasic />
-        <ForWardRefNone />
+        <ForWardRefNone /> */}
     </div>
   )
 }
