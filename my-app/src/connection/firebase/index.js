bca628c63ffd848459134d3411c589d3c879820e
Tan Do <thanhtando65@gmail.com>
2022-05-27 00:59:59 +0900

    delete firebase config

--- my-app/src/connection/firebase/index.js
+++ my-app/src/connection/firebase/index.js
@@ -2,13 +2,7 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth';
 
 const fbConfig = {
-  apiKey: "AIzaSyD1lzNvJYMaSb3x0XKnm51x6lnJ_D3qvVg",
-  authDomain: "my-web-tanlo.firebaseapp.com",
-  projectId: "my-web-tanlo",
-  storageBucket: "my-web-tanlo.appspot.com",
-  messagingSenderId: "907200977139",
-  appId: "1:907200977139:web:cfc10b7df7f298da933f86",
-  measurementId: "G-DTJL9QKK2D"
+
 }
 
 export const fbApp = initializeApp(fbConfig);
